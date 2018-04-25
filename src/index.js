31a5228eb9fc52829d7a53f66817a9123e3569d3
musicoooool@gmail.com <musicoooool@gmail.com>
2018-04-25 18:33:58 +0800

    完善调试功能

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ let newProc = require("./task/newProc");
 let abnormalProd = require("./task/abnormalProd");
 
 // 人工验证(该部分处理逻辑移至前台，此处不再做测试)
-let manualCheck = require("./task/manualCheck");
+// let manualCheck = require("./task/manualCheck");
 
 // 立体库测试
-let wms = require("./wmsTest");
+// let wms = require("./wmsTest");
 
 // 机台通知连续废
 let multiWeak = require("./task/multiWeak");
 
 const init = async () => {
   // 调试完毕
-  // await abnormalProd.init();
+  await abnormalProd.init();
   // 调试完毕
-  await wms.init().catch(e => console.log(e));
+  // await wms.init().catch(e => console.log(e));
   // 前台处理，无需后端扫描
   // await manualCheck.init();
   // await newProc.init().catch(e => console.log(e));
