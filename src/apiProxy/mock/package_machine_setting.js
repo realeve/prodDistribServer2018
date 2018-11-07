24315b2b699a92c24411c17cd7981495a6a9117b
realeve@qq.com <realeve@qq.com>
2018-11-08 00:28:12 +0800

    检封任务分配

---
+++ src/apiProxy/mock/package_machine_setting.js
@@ -0,0 +1,58 @@
+// 任务设置
+module.exports = [{
+    id: 5,
+    prod_name: '9602A',
+    machine_name: '设备1',
+    worktype_name: '白班',
+    num: 8,
+    prod_date_name: '2018-11-08'
+}, {
+    id: 6,
+    prod_name: '9602A',
+    machine_name: '设备2',
+    worktype_name: '白班',
+    num: 8,
+    prod_date_name: '2018-11-08'
+}, {
+    id: 7,
+    prod_name: '9602A',
+    machine_name: '设备3',
+    worktype_name: '中班',
+    num: 8,
+    prod_date_name: '2018-11-08'
+}, {
+    id: 8,
+    prod_name: '9602A',
+    machine_name: '设备4',
+    worktype_name: '中班',
+    num: 8,
+    prod_date_name: '2018-11-08'
+}, {
+    id: 9,
+    prod_name: '9607T',
+    machine_name: '设备5',
+    worktype_name: '白班',
+    num: 7,
+    prod_date_name: '2018-11-08'
+}, {
+    id: 10,
+    prod_name: '9607T',
+    machine_name: '设备6',
+    worktype_name: '白班',
+    num: 7,
+    prod_date_name: '2018-11-08'
+}, {
+    id: 11,
+    prod_name: '9607T',
+    machine_name: '设备7',
+    worktype_name: '中班',
+    num: 7,
+    prod_date_name: '2018-11-08'
+}, {
+    id: 12,
+    prod_name: '9607T',
+    machine_name: '设备8',
+    worktype_name: '中班',
+    num: 7,
+    prod_date_name: '2018-11-08'
+}];
--- src/apiProxy/router/package.js
+++ src/apiProxy/router/package.js
@@ -2,20 +2,121 @@ const R = require('ramda');
 const { dev } = require('../../util/axios');
 const db = require('./db_package');
 
+const getCartList = R.compose(R.flatten, R.map(R.prop('carno')));
+
 const init = async({ tstart, tend }) => {
     let { data } = await db.getVwWimWhitelist();
-    data = data.map(({ gh, prodname, carno, idx, review, lock_reason }) => {
+    data = data.map(({ gh, prodname, carno, idx, lock_reason }) => {
         idx = parseInt(idx, 10);
         return {
             gh,
             prodname,
             carno,
             idx,
-            review,
             lock_reason
         }
     });
-    return data;
+    // 未锁车车号列表
+    let unlockData = R.filter(({ lock_reason }) => R.isNil(lock_reason))(data);
+
+    let cartList = getCartList(unlockData);
+
+    let completeCarts = await getVerifyStatus(cartList);
+
+    // 图核判废完成品
+    let verifiedCarts = R.filter(({ carno }) => completeCarts.includes(carno))(unlockData);
+
+    // 从后台读取任务设置信息
+    let machineSetting = require('../mock/package_machine_setting');
+    machineSetting = handleMachineSetting(machineSetting);
+
+    let setting = getProdNum(machineSetting);
+
+    // 截取指定数量的车号
+    unlockData = getValidCarts(verifiedCarts, setting);
+
+    // 待判废车号开包量
+    let dataWithOpennum = getOpenNum(unlockData);
+
+    // 排活
+    let res = prodistCarts(dataWithOpennum, machineSetting);
+    return res;
+}
+const handleMachineSetting = machineSetting => {
+    machineSetting = R.map(item => {
+        item.num = parseInt(item.num, 10);
+        return item;
+    })(machineSetting);
+
+    return R.groupBy(R.prop('prod_name'))(machineSetting);
+}
+const getProdNum = machineSetting => {
+    return R.compose(R.map(prodname => {
+        let nums = R.compose(R.flatten, R.map(R.prop('num')))(machineSetting[prodname]);
+        let num = R.reduce(R.add, 0)(nums);
+        return {
+            prodname,
+            num
+        }
+    }), R.keys)(machineSetting);
+}
+
+// 获取每个品种指定的大万数
+const getValidCarts = (carts, setting) => {
+    carts = R.groupBy(R.prop('prodname'))(carts);
+    let res = [];
+    setting.forEach(({ prodname, num }) => {
+        let curData = carts[prodname];
+        if (!R.isNil(curData)) {
+            curData = R.slice(0, num)(curData);
+            res = [...res, ...curData]
+        }
+    })
+    return res;
+}
+
+// 车号列表判废状态
+const getVerifyStatus = async carts => {
+    // 获取车号判废状态结果，确保所有产品已执行完图像判废；
+    return carts;
+}
+
+// 单万产品开包量
+const getOpenNumByCart = cart => {
+    return parseInt(Math.random() * 100 + 50);
+}
+
+// 批量获取开包量
+const getOpenNum = async carts => {
+    // 车号列表
+    let cart = getCartList(carts);
+    // 从服务端获取开包量信息
+
+    // 模拟获取对应车号开包量信息
+    carts = carts.map(item => {
+        item.opennum = getOpenNumByCart();
+        return item;
+    })
+    return carts;
+}
+
+// 任务分配
+const prodistCarts = (carts, setting) => {
+    R.compose(R.map(
+        prodname => {
+            let cartList = R.filter(R.propEq('prodname', prodname))(carts)
+            return prodistCartByProd(cartList, setting[prodname])
+        }
+    ), R.keys)(setting)
+}
+
+/**
+ * 根据品种分配任务
+ * carts:车号列表，仅包含单一品种
+ * setting:对应该品种需要分配到几个班次
+ */
+const prodistCartByProd = (carts, setting) => {
+
 }
 
 module.exports.dev = dev;
