c924eca78f5bb00850fe1808d2a2c10149b2ff03
realeve <realeve@qq.com>
2020-07-23 17:43:22 +0800

    2T 自动置判废

--- src/sync/autoLock.js
+++ src/sync/autoLock.js
@@ -3,6 +3,20 @@ let moment = require("moment");
 
 let task_name = "3T对印自动锁图";
 
+/*
+ 2020-07-01 马可需求
+ 1. 1280,1296,4096分别代表 对印圈，对印5，黑图（补图及严重缺陷）置二次实废——8
+ 2. 除此之外的，同时 item_flag = 0(未判废)的产品，置为人工误废（1）
+ 3. 需要处理荧光缺陷（检测路数route_sheet_number为 15X，及17X），不能自动置人工误废。
+
+ 系统自动处理：
+ 1.对印及黑图实废
+ 2.机台已经判过的折纸不处理（结果为5或6）
+ 3.荧光不处理，由图核人员判废
+ 4.涂布（22X，23X）不处理
+ 5.剩下全部置误废
+*/
+
 const getCbpcAutosyncLog = () =>
   axios({
     url: "/1030/20b3a06389.json",
@@ -10,11 +24,11 @@ const getCbpcAutosyncLog = () =>
       task_type: task_name,
     },
   });
-const addCbpcAutosyncLog = (affected_rows) =>
+const addCbpcAutosyncLog = (affected_rows, type) =>
   axios({
     url: "/1031/5039fac3a0.json",
     params: {
-      type: task_name,
+      type,
       affected_rows,
     },
   });
@@ -27,6 +41,15 @@ const addCbpcAutosyncLog = (affected_rows) =>
 const setQfmQaInspectSlave = () =>
   axios({
     url: "/1029/4a5c718c88.json",
+  });
+
+/**
+ *   @database: { 全幅面 }
+ *   @desc:     { 昨日生产的3T对应产品置为二次误废 }
+ */
+const setQfmQaInspectSlaveNotFake = () =>
+  axios({
+    url: "/1110/e9d815c9ea.json",
   });
 
 // 是否需要记录
@@ -57,11 +80,21 @@ const init = async () => {
   let {
     data: [{ affected_rows }],
   } = await setQfmQaInspectSlave();
+
   console.log("锁图处理完成");
   if (affected_rows > 0) {
-    return await addCbpcAutosyncLog(affected_rows);
+    await addCbpcAutosyncLog(affected_rows, "3T对印自动锁图");
   }
-  return false;
+
+  // 置误废
+  let {
+    data: [{ affected_rows: rows2 }],
+  } = await setQfmQaInspectSlaveNotFake();
+  if (rows2 > 0) {
+    await addCbpcAutosyncLog(affected_rows, "3T对印置误废");
+  }
+
+  return affected_rows + rows2 > 0;
 };
 
 module.exports = { init };
--- src/task/tubu/index.js
+++ src/task/tubu/index.js
@@ -2,27 +2,28 @@ let { axios, mock, _commonData, DEV } = require("../../util/axios");
 let lib = require("../../util/lib");
 let R = require("ramda");
 
-const prodList = ["9604T"];
+// 全部品种
+const prodList = ["9604T", "9602T"];
 
 const moment = require("moment");
 
 // 2020-03-23 李宾
 // 9604T品，印码下机产品全部置为已判废，涂布下机置为未判废，图核完工后置为已判废
 
-const getVCbpcCartlist = prod =>
+const getVCbpcCartlist = (prod) =>
   axios({
     url: "/892/1c5169d5c1.json",
     params: {
-      prod: prodList
-    }
+      prod: prodList,
+    },
   });
 
-const getPrintMesTubuProc = type =>
+const getPrintMesTubuProc = (type) =>
   axios({
     url: "/891/314daf37f1.json",
     params: {
-      type
-    }
+      type,
+    },
   });
 
 /**
@@ -31,14 +32,14 @@ const getPrintMesTubuProc = type =>
 	以下参数在建立过程中与系统保留字段冲突，已自动替换:
 	@desc:批量插入数据时，约定使用二维数组values参数，格式为[{carts,type }]，数组的每一项表示一条数据*/
 
-const addPrintMesTubuProc = values =>
+const addPrintMesTubuProc = (values) =>
   axios({
     method: "post",
     data: {
       values,
       id: 893,
-      nonce: "809ed94180"
-    }
+      nonce: "809ed94180",
+    },
   });
 
 /** 数据量较大时建议使用post模式：
@@ -47,17 +48,17 @@ const addPrintMesTubuProc = values =>
 *   @desc:     { 调整在库产品判废状态 } 
     const { wastestatus, carno } = params;
 */
-const setUdtTbWipinventory = params =>
+const setUdtTbWipinventory = (params) =>
   axios({
     method: "post",
     data: {
       ...params,
       id: 894,
-      nonce: "d9dc8ac483"
-    }
+      nonce: "d9dc8ac483",
+    },
   });
 
-const getCarts = proc =>
+const getCarts = (proc) =>
   R.compose(R.pluck("cart_number"), R.filter(R.propEq("proc_name", proc)));
 
 /**
@@ -68,7 +69,7 @@ const getCarts = proc =>
 const WASTE_STATUS = {
   NOT_JUDGE: 0, //不判废
   WAITING: 1, //待判废
-  COMPLETE: 2 //已判废
+  COMPLETE: 2, //已判废
 };
 
 module.exports.init = async () => {
@@ -84,7 +85,7 @@ module.exports.init = async () => {
   // TODO 当天已经处理?(此处需要与图核完工品处理逻辑做区分，否则需要单独在10.9.5.133的数据库中做记录，或者在MES中新建表记录涂布的处理状态;)
   let task = {
     code: false,
-    tubu: false
+    tubu: false,
   };
 
   let res = await getPrintMesTubuProc("印码下机");
@@ -100,9 +101,7 @@ module.exports.init = async () => {
     return;
   }
 
-  const tstart = moment()
-    .subtract(1, "days")
-    .format("YYYYMMDD");
+  const tstart = moment().subtract(1, "days").format("YYYYMMDD");
 
   //  TODO 获取印码生产列表、获取涂布生产列表（04T品）
   // TODO 分离涂布以及印码产品(分别处理完工以及未完工)
@@ -112,7 +111,7 @@ module.exports.init = async () => {
   if (rows == 0) {
     await addPrintMesTubuProc([
       { carts: "当日无判废记录", type: "印码下机" },
-      { carts: "当日无判废记录", type: "涂布下机" }
+      { carts: "当日无判废记录", type: "涂布下机" },
     ]);
     return;
   }
@@ -128,15 +127,15 @@ module.exports.init = async () => {
   if (tubuList.length > 0) {
     // 涂布置为待判废
     let {
-      data: [{ affected_rows: mes_id }]
+      data: [{ affected_rows: mes_id }],
     } = await setUdtTbWipinventory({
       wastestatus: WASTE_STATUS.WAITING,
-      carno: tubuList
+      carno: tubuList,
     });
 
     if (mes_id) {
       await addPrintMesTubuProc([
-        { carts: tubuList.join(","), type: "涂布下机" }
+        { carts: tubuList.join(","), type: "涂布下机" },
       ]);
     }
   }
@@ -144,15 +143,15 @@ module.exports.init = async () => {
   if (codeList.length > 0) {
     // 记录状态
     let {
-      data: [{ affected_rows: mes_id2 }]
+      data: [{ affected_rows: mes_id2 }],
     } = await setUdtTbWipinventory({
       wastestatus: WASTE_STATUS.NOT_JUDGE,
-      carno: codeList
+      carno: codeList,
     });
 
     if (mes_id2) {
       await addPrintMesTubuProc([
-        { carts: codeList.join(","), type: "印码下机" }
+        { carts: codeList.join(","), type: "印码下机" },
       ]);
     }
   }
